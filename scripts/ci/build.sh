6d50e5bc6060f7b82932a1d3fd159f82d08b383f
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2023-02-02 17:34:03 +0100

    Prevent overwrite of commit hash

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -20,10 +20,11 @@ else
     TZ=UTC git show --quiet --date="format-local:%Y.%-m.%-d" --format="${prerelease_source}.%cd" >prerelease.txt
 fi
 
-if [ -n "$CIRCLE_SHA1" ]
-then
-    echo -n "$CIRCLE_SHA1" >commit_hash.txt
-fi
+# We don't want to overwrite the commit for this bug fix
+# if [ -n "$CIRCLE_SHA1" ]
+# then
+#     echo -n "$CIRCLE_SHA1" >commit_hash.txt
+# fi
 
 mkdir -p build
 cd build
