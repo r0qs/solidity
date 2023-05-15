be4766c0a0eb72912b986b7d04fb2dc3299c48d3
r0qs <rodrigo.saramago@ethereum.org>
2023-05-15 22:34:26 +0200

    Force error

--- .circleci/build_win.ps1
+++ .circleci/build_win.ps1
@@ -15,7 +15,6 @@ else {
 	-join("ci.", $last_commit_date) | out-file -encoding ascii prerelease.txt
 }
 
-mkdir build
 cd build
 $boost_dir=(Resolve-Path $PSScriptRoot\..\deps\boost\lib\cmake\Boost-*)
 ..\deps\cmake\bin\cmake -G "Visual Studio 16 2019" -DBoost_DIR="$boost_dir\" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\upload" -DUSE_Z3=OFF ..
